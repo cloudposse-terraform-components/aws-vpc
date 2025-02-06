a2636978d204356242ccda0ae9eff279b72f6520
Igor Rodionov <goruha@gmail.com>
2025-02-06 20:40:17 +0100

    Pin version

--- src/providers.tf
+++ src/providers.tf
@@ -3,9 +3,7 @@ provider "aws" {
 
   # Profile is deprecated in favor of terraform_role_arn. When profiles are not in use, terraform_profile_name is null.
   profile = module.iam_roles.terraform_profile_name
-  default_tags {
-    tags = var.default_tags
-  }
+
   dynamic "assume_role" {
     # module.iam_roles.terraform_role_arn may be null, in which case do not assume a role.
     for_each = compact([module.iam_roles.terraform_role_arn])
@@ -18,10 +16,4 @@ provider "aws" {
 module "iam_roles" {
   source  = "../account-map/modules/iam-roles"
   context = module.this.context
-}
-
-variable "default_tags" {
-  type        = map(string)
-  description = "A map of tags to add to every resource"
-  default     = {}
 }
