71cd797b5ba8d8e28ae3bc1f2212786190ecd3e6
Igor Rodionov <goruha@gmail.com>
2025-02-19 22:29:43 +0300

    Added tests (#10)
    * Added tests
    
    * Use reference to account-map for asserts
    
    * Added versions.tf
    
    * Added minimal and full test
    
    * Remove non working settings
    
    * Update helpers version
    
    * Added asserts
    
    * Added terratests
    
    * Added terratests
    
    * Added tests as separate step
    
    * Fix go.mod
    
    * Fix go.sum
    
    * Test dependency workflows
    
    * Added test for public vpc
    
    * Rename catalog dir
    
    * Remove parallel execution
    
    * refactor tests to use new test-helpers lib (#19)
    
    * refactor tests to use new test-helpers lib
    
    * Redo tests
    
    ---------
    
    Co-authored-by: Igor Rodionov <goruha@gmail.com>
    
    * Pin version
    
    * Pin version
    
    * Pin version
    
    * Fix tests
    
    * Added test
    
    * Fix test
    
    * Added component test
    
    * Pin version
    
    * Pin helpers
    
    * Remove trunk code
    
    ---------
    
    Co-authored-by: Matt Calhoun <matt@mattcalhoun.com>

---
+++ test/component_test.go
@@ -0,0 +1,132 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cloudposse/test-helpers/pkg/atmos"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	awshelper "github.com/cloudposse/test-helpers/pkg/aws"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type ComponentSuite struct {
+	helper.TestSuite
+}
+
+func (s *ComponentSuite) TestPrivateVPC() {
+	const component = "vpc/private"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	cidrBlock := atmos.Output(s.T(), options, "vpc_cidr")
+
+	assert.Equal(s.T(), "172.16.0.0/16", cidrBlock)
+
+	vpcId := atmos.Output(s.T(), options, "vpc_id")
+	require.True(s.T(), strings.HasPrefix(vpcId, "vpc-"))
+
+	vpc := aws.GetVpcById(s.T(), vpcId, awsRegion)
+
+	assert.Equal(s.T(), vpc.Name, fmt.Sprintf("eg-default-ue2-test-vpc-terraform-%s", s.Config.RandomIdentifier))
+	assert.Equal(s.T(), *vpc.CidrAssociations[0], "172.16.0.0/16")
+	assert.Equal(s.T(), *vpc.CidrBlock, "172.16.0.0/16")
+	assert.Nil(s.T(), vpc.Ipv6CidrAssociations)
+	assert.Equal(s.T(), vpc.Tags["Environment"], "ue2")
+	assert.Equal(s.T(), vpc.Tags["Namespace"], "eg")
+	assert.Equal(s.T(), vpc.Tags["Stage"], "test")
+	assert.Equal(s.T(), vpc.Tags["Tenant"], "default")
+
+	subnets := vpc.Subnets
+	require.Equal(s.T(), 2, len(subnets))
+
+	public_subnet_ids := atmos.OutputList(s.T(), options, "public_subnet_ids")
+	assert.Empty(s.T(), public_subnet_ids)
+
+	public_subnet_cidrs := atmos.OutputList(s.T(), options, "public_subnet_cidrs")
+	assert.Empty(s.T(), public_subnet_cidrs)
+
+	private_subnet_ids := atmos.OutputList(s.T(), options, "private_subnet_ids")
+	assert.Equal(s.T(), 2, len(private_subnet_ids))
+
+	assert.Contains(s.T(), private_subnet_ids, subnets[0].Id)
+	assert.Contains(s.T(), private_subnet_ids, subnets[1].Id)
+
+	assert.False(s.T(), aws.IsPublicSubnet(s.T(), subnets[0].Id, awsRegion))
+	assert.False(s.T(), aws.IsPublicSubnet(s.T(), subnets[1].Id, awsRegion))
+
+	nats, err := awshelper.GetNatGatewaysByVpcIdE(s.T(), context.Background(), vpcId, awsRegion)
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 0, len(nats))
+
+	s.DriftTest(component, stack, nil)
+}
+
+func (s *ComponentSuite) TestPublicVPC() {
+	const component = "vpc/public"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	cidrBlock := atmos.Output(s.T(), options, "vpc_cidr")
+	assert.Equal(s.T(), "172.16.0.0/16", cidrBlock)
+
+	vpcId := atmos.Output(s.T(), options, "vpc_id")
+	require.True(s.T(), strings.HasPrefix(vpcId, "vpc-"))
+
+	vpc := aws.GetVpcById(s.T(), vpcId, awsRegion)
+
+	assert.Equal(s.T(), vpc.Name, fmt.Sprintf("eg-default-ue2-test-vpc-terraform-%s", s.Config.RandomIdentifier))
+	assert.Equal(s.T(), *vpc.CidrAssociations[0], "172.16.0.0/16")
+	assert.Equal(s.T(), *vpc.CidrBlock, "172.16.0.0/16")
+	assert.Nil(s.T(), vpc.Ipv6CidrAssociations)
+	assert.Equal(s.T(), vpc.Tags["Environment"], "ue2")
+	assert.Equal(s.T(), vpc.Tags["Namespace"], "eg")
+	assert.Equal(s.T(), vpc.Tags["Stage"], "test")
+	assert.Equal(s.T(), vpc.Tags["Tenant"], "default")
+
+	subnets := vpc.Subnets
+	require.Equal(s.T(), 4, len(subnets))
+
+	public_subnet_ids := atmos.OutputList(s.T(), options, "public_subnet_ids")
+	assert.Equal(s.T(), 2, len(public_subnet_ids))
+
+	public_subnet_cidrs := atmos.OutputList(s.T(), options, "public_subnet_cidrs")
+	assert.Equal(s.T(), 2, len(public_subnet_cidrs))
+
+	private_subnet_ids := atmos.OutputList(s.T(), options, "private_subnet_ids")
+	assert.Equal(s.T(), 2, len(private_subnet_ids))
+
+	private_subnet_cidrs := atmos.OutputList(s.T(), options, "private_subnet_cidrs")
+	assert.Equal(s.T(), 2, len(private_subnet_cidrs))
+
+	assert.False(s.T(), aws.IsPublicSubnet(s.T(), private_subnet_ids[0], awsRegion))
+	assert.False(s.T(), aws.IsPublicSubnet(s.T(), private_subnet_ids[1], awsRegion))
+
+	assert.True(s.T(), aws.IsPublicSubnet(s.T(), public_subnet_ids[0], awsRegion))
+	assert.True(s.T(), aws.IsPublicSubnet(s.T(), public_subnet_ids[1], awsRegion))
+
+	nats, err := awshelper.GetNatGatewaysByVpcIdE(s.T(), context.Background(), vpcId, awsRegion)
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 1, len(nats))
+
+	s.DriftTest(component, stack, nil)
+}
+
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "vpc/disabled"
+	const stack = "default-test"
+	s.VerifyEnabledFlag(component, stack, nil)
+}
+
+func TestRunVPCSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+	helper.Run(t, suite)
+}
