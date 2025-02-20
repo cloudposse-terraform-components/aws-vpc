0bd1789a7c69a384e0b25923aad51784be20c590
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-02-20 15:51:02 -0500

    chore: add tests for vpc flow logs

--- test/component_test.go
+++ test/component_test.go
@@ -120,6 +120,64 @@ func (s *ComponentSuite) TestPublicVPC() {
 	s.DriftTest(component, stack, nil)
 }
 
+func (s *ComponentSuite) TestVPCFlowLogs() {
+	const component = "vpc/with_flowlogs"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	cidrBlock := atmos.Output(s.T(), options, "vpc_cidr")
+	assert.Equal(s.T(), "172.16.0.0/16", cidrBlock)
+
+	vpcId := atmos.Output(s.T(), options, "vpc_id")
+	require.True(s.T(), strings.HasPrefix(vpcId, "vpc-"))
+
+	vpc := aws.GetVpcById(s.T(), vpcId, awsRegion)
+
+	assert.Equal(s.T(), vpc.Name, fmt.Sprintf("eg-default-ue2-test-vpc-terraform-%s", s.Config.RandomIdentifier))
+	assert.Equal(s.T(), *vpc.CidrAssociations[0], "172.16.0.0/16")
+	assert.Equal(s.T(), *vpc.CidrBlock, "172.16.0.0/16")
+	assert.Nil(s.T(), vpc.Ipv6CidrAssociations)
+	assert.Equal(s.T(), vpc.Tags["Environment"], "ue2")
+	assert.Equal(s.T(), vpc.Tags["Namespace"], "eg")
+	assert.Equal(s.T(), vpc.Tags["Stage"], "test")
+	assert.Equal(s.T(), vpc.Tags["Tenant"], "default")
+
+	subnets := vpc.Subnets
+	require.Equal(s.T(), 4, len(subnets))
+
+	public_subnet_ids := atmos.OutputList(s.T(), options, "public_subnet_ids")
+	assert.Equal(s.T(), 2, len(public_subnet_ids))
+
+	public_subnet_cidrs := atmos.OutputList(s.T(), options, "public_subnet_cidrs")
+	assert.Equal(s.T(), 2, len(public_subnet_cidrs))
+
+	private_subnet_ids := atmos.OutputList(s.T(), options, "private_subnet_ids")
+	assert.Equal(s.T(), 2, len(private_subnet_ids))
+
+	private_subnet_cidrs := atmos.OutputList(s.T(), options, "private_subnet_cidrs")
+	assert.Equal(s.T(), 2, len(private_subnet_cidrs))
+
+	assert.False(s.T(), aws.IsPublicSubnet(s.T(), private_subnet_ids[0], awsRegion))
+	assert.False(s.T(), aws.IsPublicSubnet(s.T(), private_subnet_ids[1], awsRegion))
+
+	assert.True(s.T(), aws.IsPublicSubnet(s.T(), public_subnet_ids[0], awsRegion))
+	assert.True(s.T(), aws.IsPublicSubnet(s.T(), public_subnet_ids[1], awsRegion))
+
+	nats, err := awshelper.GetNatGatewaysByVpcIdE(s.T(), context.Background(), vpcId, awsRegion)
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 1, len(nats))
+
+	flow_log_destination := atmos.OutputList(s.T(), options, "flow_log_destination")
+	assert.Equal(s.T(), 1, len(flow_log_destination))
+
+	flow_log_id := atmos.OutputList(s.T(), options, "flow_log_id")
+	assert.Equal(s.T(), 1, len(flow_log_id))
+
+	s.DriftTest(component, stack, nil)
+}
+
 func (s *ComponentSuite) TestEnabledFlag() {
 	const component = "vpc/disabled"
 	const stack = "default-test"
