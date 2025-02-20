c752eaf00a52eced8507bcd7cb2de329bfa4989d
RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
2025-02-20 06:46:17 -0500

    feat: parameterize vpc flow logs bucket, add destination options, and expand log destination settings (#23)
    * feat: parameterize vpc flow logs bucket, add destination options, and
    adjust log destination
    
    * docs: update readme docs with variables

--- src/vpc-flow-logs.tf
+++ src/vpc-flow-logs.tf
@@ -1,11 +1,21 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/flow_log
+locals {
+  account_name    = lookup(module.this.descriptors, "account_name", module.this.stage)
+  log_destination = format("%v/%v/%v/", one(module.vpc_flow_logs_bucket[*].outputs.vpc_flow_logs_bucket_arn), local.account_name, module.this.id)
+}
 
 resource "aws_flow_log" "default" {
   count                = local.vpc_flow_logs_enabled ? 1 : 0
-  log_destination      = module.vpc_flow_logs_bucket[0].outputs.vpc_flow_logs_bucket_arn
+  log_destination      = local.log_destination
   log_destination_type = var.vpc_flow_logs_log_destination_type
   traffic_type         = var.vpc_flow_logs_traffic_type
   vpc_id               = module.vpc.vpc_id
+
+  destination_options {
+    file_format                = var.vpc_flow_logs_destination_options_file_format
+    hive_compatible_partitions = var.vpc_flow_logs_destination_options_hive_compatible_partitions
+    per_hour_partition         = var.vpc_flow_logs_destination_options_per_hour_partition
+  }
 
   tags = module.this.tags
 }
