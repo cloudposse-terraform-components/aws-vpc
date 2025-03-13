1084bfe376aabc5f868caf448983676050149f1c
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-03-13 16:06:31 -0400

    fix: update remote state, add tests for vpc flow logs, and cleanup docs

--- test/component_test.go
+++ test/component_test.go
@@ -27,6 +27,11 @@ func (s *ComponentSuite) TestPrivateVPC() {
 	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
 	cidrBlock := atmos.Output(s.T(), options, "vpc_cidr")
 
+	// Ensure S3 bucket is empty before destroy
+	vpcFlowLogsBucketOptions := s.GetAtmosOptions("vpc-flow-logs-bucket", stack, nil)
+	bucketName := atmos.Output(s.T(), vpcFlowLogsBucketOptions, "vpc_flow_logs_bucket_id")
+	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
+
 	assert.Equal(s.T(), "172.16.0.0/16", cidrBlock)
 
 	vpcId := atmos.Output(s.T(), options, "vpc_id")
@@ -76,6 +81,12 @@ func (s *ComponentSuite) TestPublicVPC() {
 	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
 	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
 	cidrBlock := atmos.Output(s.T(), options, "vpc_cidr")
+
+	// Ensure S3 bucket is empty before destroy
+	vpcFlowLogsBucketOptions := s.GetAtmosOptions("vpc-flow-logs-bucket", stack, nil)
+	bucketName := atmos.Output(s.T(), vpcFlowLogsBucketOptions, "vpc_flow_logs_bucket_id")
+	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
+
 	assert.Equal(s.T(), "172.16.0.0/16", cidrBlock)
 
 	vpcId := atmos.Output(s.T(), options, "vpc_id")
@@ -126,7 +137,14 @@ func (s *ComponentSuite) TestVPCFlowLogs() {
 	const awsRegion = "us-east-2"
 
 	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+
+	// Ensure S3 bucket is empty before destroy
+	vpcFlowLogsBucketOptions := s.GetAtmosOptions("vpc-flow-logs-bucket", stack, nil)
+	bucketName := atmos.Output(s.T(), vpcFlowLogsBucketOptions, "vpc_flow_logs_bucket_id")
+	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
+
 	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+
 	cidrBlock := atmos.Output(s.T(), options, "vpc_cidr")
 	assert.Equal(s.T(), "172.16.0.0/16", cidrBlock)
 
@@ -145,35 +163,19 @@ func (s *ComponentSuite) TestVPCFlowLogs() {
 	assert.Equal(s.T(), vpc.Tags["Tenant"], "default")
 
 	subnets := vpc.Subnets
-	require.Equal(s.T(), 4, len(subnets))
-
-	public_subnet_ids := atmos.OutputList(s.T(), options, "public_subnet_ids")
-	assert.Equal(s.T(), 2, len(public_subnet_ids))
-
-	public_subnet_cidrs := atmos.OutputList(s.T(), options, "public_subnet_cidrs")
-	assert.Equal(s.T(), 2, len(public_subnet_cidrs))
-
-	private_subnet_ids := atmos.OutputList(s.T(), options, "private_subnet_ids")
-	assert.Equal(s.T(), 2, len(private_subnet_ids))
-
-	private_subnet_cidrs := atmos.OutputList(s.T(), options, "private_subnet_cidrs")
-	assert.Equal(s.T(), 2, len(private_subnet_cidrs))
-
-	assert.False(s.T(), aws.IsPublicSubnet(s.T(), private_subnet_ids[0], awsRegion))
-	assert.False(s.T(), aws.IsPublicSubnet(s.T(), private_subnet_ids[1], awsRegion))
-
-	assert.True(s.T(), aws.IsPublicSubnet(s.T(), public_subnet_ids[0], awsRegion))
-	assert.True(s.T(), aws.IsPublicSubnet(s.T(), public_subnet_ids[1], awsRegion))
+	require.Equal(s.T(), 1, len(subnets))
 
 	nats, err := awshelper.GetNatGatewaysByVpcIdE(s.T(), context.Background(), vpcId, awsRegion)
 	assert.NoError(s.T(), err)
 	assert.Equal(s.T(), 1, len(nats))
 
-	flow_log_destination := atmos.Output(s.T(), options, "flow_log_destination")
-	assert.NotEmpty(s.T(), flow_log_destination)
+	flow_log_destinations := atmos.Output(s.T(), options, "flow_log_destination")
+	require.NotEmpty(s.T(), flow_log_destinations, "Expected at least one flow log destination but found none")
+	require.True(s.T(), strings.HasPrefix(flow_log_destinations, "arn:aws:s3:::eg-default-ue2-test-vpc-flow-logs-bucket"))
 
-	flow_log_id := atmos.Output(s.T(), options, "flow_log_id")
-	assert.NotEmpty(s.T(), flow_log_id)
+	flow_log_ids := atmos.OutputList(s.T(), options, "flow_log_id")
+	require.NotEmpty(s.T(), flow_log_ids, "Expected at least one flow log ID but found none")
+	require.True(s.T(), strings.HasPrefix(flow_log_ids[0], "fl-"), "Flow log ID does not match expected format")
 
 	s.DriftTest(component, stack, nil)
 }
@@ -188,6 +190,5 @@ func TestRunVPCSuite(t *testing.T) {
 	suite := new(ComponentSuite)
 
 	suite.AddDependency(t, "vpc-flow-logs-bucket", "default-test", nil)
-
 	helper.Run(t, suite)
 }
